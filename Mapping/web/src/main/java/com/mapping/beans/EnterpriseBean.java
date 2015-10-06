5b6a79ae84346f5604c42111dc86cad5f161ea2b
juanpdui <windwarkerjp@hotmail.com>
2015-10-06 10:48:58 -0500

    asd

--- Mapping/web/src/main/java/com/mapping/beans/EnterpriseBean.java
+++ Mapping/web/src/main/java/com/mapping/beans/EnterpriseBean.java
@@ -144,9 +144,11 @@ public class EnterpriseBean implements Serializable{
 		enterprise= ent;
 		//cambio en declaracion de objeto
 		/*ent = new Enterprise();	
+		 * +
 		enterprise= ent;*/
 		//piruja
 		//asdasdasdasdasd
+		//asdasdasd
 		RequestContext.getCurrentInstance().update("enterpriseForm:insertDialog");
 		RequestContext.getCurrentInstance().execute("PF('dlg2').show();");
 	}
